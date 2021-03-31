e33110c3c353e82e9e3e69732074ab763182e099
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-03-31 19:12:53 +0200

    Show best candidates of PG2

--- pg2-ilp.sh
+++ pg2-ilp.sh
@@ -3,15 +3,20 @@
 # total time budget in seconds
 time_budget=60
 
+# number of candidates to print
+best=30
+
 exec_dir=$(readlink -f $0)
 exec_dir=${exec_dir%/*}
 
 candidates=$(mktemp)
-start=`date +%s`
+start=$(date +%s)
 $exec_dir/pg2/pg2.sh $* > $candidates
-end=`date +%s`
+end=$(date +%s)
 runtime=$((end-start))
+echo "Computed $(wc -l < $candidates) candidates"
+echo "Best $best candidates:"
+cat $candidates | sort -r -gk1,1 -t, | head -n $best
 ilp_tb=$((time_budget-runtime))
 $exec_dir/ilp/ilp $candidates $ilp_tb
-#cat $candidates | sort -gk1,1 -t, | tail -n 30
 rm $candidates
