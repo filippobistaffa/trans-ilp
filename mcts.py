058a77951c7b3405b3ffbc700dbee5efebb900c5
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-03-31 11:46:31 +0200

    Better code

--- mcts.py
+++ mcts.py
@@ -3,6 +3,7 @@ from oracle.oracle import OracleData
 from tree import MCTS, Node
 import numpy as np
 import random
+import os
 
 class Coalition(Node):
     def __init__(self, idxs, terminal):
@@ -67,7 +68,7 @@ if __name__ == '__main__':
     )
     parser.add_argument('pool', metavar='POOL', type=str, help='Pool file')
     parser.add_argument('--max_size', type=int, default=5, help='Maximum coalition size (default = 5)')
-    parser.add_argument('--task', type=str, default='data/task_english', help='Task input file')
+    parser.add_argument('--task', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'task_english'), help='Task input file')
     parser.add_argument('--seed', type=int, default=0, help='Seed (default = 0)')
     parser.add_argument('--uct', type=float, default=50, help='UCT weight (default = 50)')
     parser.add_argument('--exploration', type=float, default=0.1, help='Exploration weight (default = 0.1)')
