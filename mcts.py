ca7506ae11dffe55106e7e6b505fd2c25776447d
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-04-15 18:15:03 +0200

    Nicer code

--- mcts.py
+++ mcts.py
@@ -11,7 +11,8 @@ class Coalition(Node):
         self.terminal = terminal
 
     def remaining(self):
-        remaining = [i for i in all_idxs[:len(all_idxs)-(max_size-len(self.idxs)-1)] if not self.idxs or i > max(self.idxs)]
+        feasible = all_idxs[:len(all_idxs) - (max_size - len(self.idxs) - 1)]
+        remaining = [i for i in feasible if not self.idxs or i > max(self.idxs)]
         #remaining = [i for i in all_idxs if i not in self.idxs]
         random.shuffle(remaining)
         return remaining
