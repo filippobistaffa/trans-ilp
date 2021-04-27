fb5be8bcd548b52995f30e4828908121b67fe7e4
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-04-27 18:04:33 +0200

    Print bad solution value in case of failure

--- ilp/ilp.cpp
+++ ilp/ilp.cpp
@@ -134,6 +134,7 @@ int main(int argc, char *argv[]) {
 	try {
 		if (!cplex.solve()) {
 			std::cout << RED("Unable to find a solution") << '\n';
+			std::cout << -1e3 << '\n';
 			exit(EXIT_FAILURE);
 		}
 	}
