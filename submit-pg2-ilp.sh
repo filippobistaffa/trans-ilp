ddf6f94711e3cf04a73d6e89847209be24deede1
Filippo Bistaffa <filippo.bistaffa@iiia.csic.es>
2021-08-25 18:42:03 +0200

    Submit scripts

---
+++ submit-pg2-ilp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BEEGFS="/mnt/beegfs/iiia/filippo.bistaffa"
+ROOT_DIR="/home/filippo.bistaffa/mcts-ilp-rs"
+EXECUTABLE="$ROOT_DIR/pg2-ilp.sh"
+LOG_DIR="$BEEGFS/pmf/pg2"
+DATA_DIR="$ROOT_DIR/data"
+POOL_DIR="$DATA_DIR/pmf_50"
+
+mkdir -p $LOG_DIR
+STDOUT=$LOG_DIR/$1-$2.stdout
+STDERR=$LOG_DIR/$1-$2.stderr
+
+tmpfile=$(mktemp)
+sbatch 1> $tmpfile <<EOF
+#!/bin/bash
+#SBATCH --job-name=pg2-$1-$2
+#SBATCH --partition=quick
+#SBATCH --time=5:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --output=/dev/null
+#SBATCH --error=/dev/null
+echo $EXECUTABLE $POOL_DIR/$1.csv $2 1> $STDOUT
+srun $EXECUTABLE $POOL_DIR/$1.csv $2 1>> $STDOUT 2>> $STDERR
+RET=\$?
+exit \$RET
+EOF
