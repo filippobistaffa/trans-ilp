d1e0bc69d5f4585f996415ca5863c5d77b6c21d5
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-10-25 13:07:31 +0200

    Updated test

--- oracle/test.py
+++ oracle/test.py
@@ -7,3 +7,7 @@ data = OracleData('../data/pool.json', '../data/task_english')
 team = sys.argv[1:]
 value = oracle(np.array(team, dtype=np.uint32), data)
 print(value)
+
+data.sample_agents(50, 0)
+value = oracle(np.array(team, dtype=np.uint32), data)
+print(value)
