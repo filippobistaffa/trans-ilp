51b9d183a3cad5c506440750589b8e3ccc293c84
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-11-04 18:15:20 +0100

    Updated submit script for synteam

--- submit-synteam.sh
+++ submit-synteam.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-EXEC_DIR="/home/filippo.bistaffa/mcts-ilp-tf/synteam"
+if hash condor_submit 2>/dev/null
+then
+
+HOME="/lhome/ext/iiia021/iiia0211"
+ROOT_DIR="$HOME/trans-ilp-tf"
+EXEC_DIR="$ROOT_DIR/synteam"
+EXECUTABLE="$EXEC_DIR/synteam.sh"
+LOG_DIR="$HOME/log/tf/synteam"
+
+mkdir -p $LOG_DIR
+STDOUT=$LOG_DIR/$1-$2.stdout
+STDLOG=$LOG_DIR/$1-$2.stdlog
+STDERR=$LOG_DIR/$1-$2.stderr
+
+tmpfile=$(mktemp)
+condor_submit 1> $tmpfile <<EOF
+universe = vanilla
+stream_output = True
+stream_error = True
+initialdir = $EXEC_DIR
+executable = $EXECUTABLE
+arguments = $1 $2
+log = $STDLOG
+output = $STDOUT
+error = $STDERR
+queue
+EOF
+
+elif hash sbatch 2>/dev/null
+then
+
+HOME="/home/filippo.bistaffa"
+ROOT_DIR="$HOME/trans-ilp-tf"
+EXEC_DIR="$ROOT_DIR/synteam"
 EXECUTABLE="./synteam.sh"
-BEEGFS="/mnt/beegfs/iiia/filippo.bistaffa"
+BEEGFS="$HOME/beegfs"
 LOG_DIR="$BEEGFS/tf/synteam"
 
 mkdir -p $LOG_DIR
@@ -25,3 +58,7 @@ srun $EXECUTABLE $1 $2 1> $STDOUT 2> $STDERR
 RET=\$?
 exit \$RET
 EOF
+
+else
+echo "Unknown cluster"
+fi
