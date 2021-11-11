bf62dd3feef9e32c215607c0a27473a37fd9fe67
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-11-11 10:58:18 +0100

    --budget and --generation parameters also for PG2

--- pg2-ilp.sh
+++ pg2-ilp.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 
-# total time budget in seconds
-time_budget=60
+# default total time budget in seconds
+budget=60
 
 # number of candidates to print
 best=30
 
+args=""
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+    case $key in
+        --budget)
+            shift
+            budget="$1"
+            shift
+        ;;
+        *)
+            args="$args$key "
+            shift
+        ;;
+    esac
+done
+
 exec_dir=$(readlink -f $0)
 exec_dir=${exec_dir%/*}
-
 candidates=$(mktemp)
 start=$(date +%s)
-$exec_dir/pg2/pg2.sh $* > $candidates
+$exec_dir/pg2/pg2.sh $args > $candidates
+ret=$?
+if [ $ret -ne 0 ]
+then
+    exit 1
+fi
 end=$(date +%s)
 runtime=$((end-start))
-echo "Computed $(wc -l < $candidates) candidates"
+echo "Computed $(wc -l < $candidates) candidates in $runtime seconds"
 echo "Best $best candidates:"
 cat $candidates | LC_ALL=C sort -r -gk1,1 -t, | head -n $best
-ilp_tb=$((time_budget-runtime))
-$exec_dir/ilp/ilp $candidates $ilp_tb
+tilp=$((budget-runtime))
+$exec_dir/ilp/ilp $candidates $tilp
 rm $candidates
--- pg2/pg2.sh
+++ pg2/pg2.sh
@@ -2,20 +2,40 @@
 
 exec_dir=$(readlink -f $0)
 exec_dir=${exec_dir%/*}
-
 distance_matrix="$exec_dir/../data/gmaps_distance.csv"
 time_matrix="$exec_dir/../data/gmaps_time.csv"
-time_budget=60
-generation_portion=0.66666666
+generation_time_budget=40
 determinism_rate=0.62
 candidate_list_length=6
 seed=$RANDOM
 env_k=0.5
 threads=1
 
-if [ $# -eq 2 ]
-then
-        seed=$2
-fi
+while [[ $# > 0 ]]
+do
+    key="$1"
+    case $key in
+        --generation)
+            shift
+            generation_time_budget="$1"
+            shift
+        ;;
+        --seed)
+            shift
+            seed="$1"
+            shift
+        ;;
+        *)
+            instance="$1"
+            shift
+        ;;
+    esac
+done
 
-$exec_dir/pg2 $1 ${distance_matrix} ${time_matrix} ${seed} ${time_budget} ${generation_portion} ${determinism_rate} ${candidate_list_length} ${threads} ${env_k}
+if test "${instance}" && file ${instance} | grep -q "ASCII text"
+then
+    ${exec_dir}/pg2 ${instance} ${distance_matrix} ${time_matrix} ${seed} ${generation_time_budget} ${determinism_rate} ${candidate_list_length} ${threads} ${env_k}
+else
+    echo "Instance CSV file not found!"
+    exit 1
+fi
