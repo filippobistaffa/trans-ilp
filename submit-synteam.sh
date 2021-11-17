4dca2da70c6a04ad2ffe846304fcf7d06da6761a
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-11-17 17:06:52 +0100

    Updated submit scripts

--- submit-synteam.sh
+++ submit-synteam.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-LAMBDA=${3:-0.8}
+k=5
+n=50
+lambda=0.8
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+    case $key in
+        -n)
+            shift
+            n="$1"
+            shift
+        ;;
+        -l|--lambda)
+            shift
+            lambda="$1"
+            shift
+        ;;
+    esac
+done
 
 if hash condor_submit 2>/dev/null
 then
@@ -9,12 +28,12 @@ HOME="/lhome/ext/iiia021/iiia0211"
 ROOT_DIR="$HOME/trans-ilp-tf"
 EXEC_DIR="$ROOT_DIR/synteam"
 EXECUTABLE="$EXEC_DIR/synteam.sh"
-LOG_DIR="$HOME/log/tf/$1-synteam"
+LOG_DIR="$HOME/log/tf/$n-$lambda-synteam"
 
 mkdir -p $LOG_DIR
-STDOUT=$LOG_DIR/$2-$LAMBDA.stdout
-STDLOG=$LOG_DIR/$2-$LAMBDA.stdlog
-STDERR=$LOG_DIR/$2-$LAMBDA.stderr
+STDOUT=$LOG_DIR/all.stdout
+STDLOG=$LOG_DIR/all.stdlog
+STDERR=$LOG_DIR/all.stderr
 
 tmpfile=$(mktemp)
 condor_submit 1> $tmpfile <<EOF
@@ -23,7 +42,7 @@ stream_output = True
 stream_error = True
 initialdir = $EXEC_DIR
 executable = $EXECUTABLE
-arguments = $1 $2 $LAMBDA
+arguments = $n $k $lambda
 log = $STDLOG
 output = $STDOUT
 error = $STDERR
@@ -38,16 +57,16 @@ ROOT_DIR="$HOME/trans-ilp-tf"
 EXEC_DIR="$ROOT_DIR/synteam"
 EXECUTABLE="./synteam.sh"
 BEEGFS="$HOME/beegfs"
-LOG_DIR="$BEEGFS/tf/$1-synteam"
+LOG_DIR="$BEEGFS/tf/$n-$lambda-synteam"
 
 mkdir -p $LOG_DIR
-STDOUT=$LOG_DIR/$2-$LAMBDA.stdout
-STDERR=$LOG_DIR/$2-$LAMBDA.stderr
+STDOUT=$LOG_DIR/all.stdout
+STDERR=$LOG_DIR/all.stderr
 
 tmpfile=$(mktemp)
 sbatch 1> $tmpfile <<EOF
 #!/bin/bash
-#SBATCH --job-name=synteam-$1-$2-$LAMBDA
+#SBATCH --job-name=synteam-$1-$2-$lambda
 #SBATCH --partition=general-new
 #SBATCH --time=6:00:00
 #SBATCH --ntasks=1
@@ -56,7 +75,7 @@ sbatch 1> $tmpfile <<EOF
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
 #SBATCH --chdir=$EXEC_DIR
-srun $EXECUTABLE $1 $2 $LAMBDA 1> $STDOUT 2> $STDERR
+srun $EXECUTABLE $n $k $lambda 1> $STDOUT 2> $STDERR
 RET=\$?
 exit \$RET
 EOF
