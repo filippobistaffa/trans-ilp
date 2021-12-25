36090ae0c9f35ee40bbcfe2ab070d5cbec83060f
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2021-12-25 21:34:50 +0100

    Updated loop submit script

--- loop.sh
+++ loop.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
+function wait_empty_queue {
+    while true
+    do
+        if hash condor_submit 2>/dev/null
+        then
+            remaining=$( condor_q | tail -n 3 | head -n 1 | cut -f 4 -d\  )
+        elif hash sbatch 2>/dev/null
+        then
+            remaining=$( squeue -h | wc -l )
+        else
+            echo "Unknown cluster"
+            exit 1
+        fi
+        #echo "$remaining jobs remaining" 
+        if [[ $remaining == 0 ]]
+        then
+            return
+        else
+            #echo "Waiting queue to be empty..." 
+            sleep 60
+        fi
+    done
+}
+
 n=50
 start=0
-end=200
+end=199
 seeds=50
 pg2=false
 trans=false
+step=200
 
 while [[ $# > 0 ]]
 do
@@ -52,41 +77,33 @@ then
     exit 1
 fi
 
-if hash condor_submit 2>/dev/null
-then
-    step=$end
-    delay=0
-elif hash sbatch 2>/dev/null
-then
-    step=200
-    delay=600
-else
-    echo "Unknown cluster"
-    exit 1
-fi
+j=0
 
-i=$start
-
-while true
+for i in $( seq $start $end )
 do
-    for j in $( seq 1 $step )
+    for s in $( seq 0 $(( $seeds - 1 )) )
     do
-        if [[ $i == $end ]]
+        if [[ $j == $step ]]
         then
-            exit
+            wait_empty_queue
+            j=0
         fi
-        for s in $( seq 0 $(( $seeds - 1 )) )
-        do
-            if [ "$trans" = true ]
-            then
-                ./submit-trans-ilp.sh -n $n -i $i -s $s $args
-            fi
-            if [ "$pg2" = true ]
-            then
-                ./submit-pg2-ilp.sh -n $n -i $i -s $s $args
-            fi
-        done
-        i=$((i+1))
+        if [ "$trans" = true ]
+        then
+            #echo "Submitted Trans ($i $s)"
+            ./submit-trans-ilp.sh -n $n -i $i -s $s $args
+            j=$((j+1))
+        fi
+        if [[ $j == $step ]]
+        then
+            wait_empty_queue
+            j=0
+        fi
+        if [ "$pg2" = true ]
+        then
+            #echo "Submitted PG2 ($i $s)"
+            ./submit-pg2-ilp.sh -n $n -i $i -s $s $args
+            j=$((j+1))
+        fi
     done
-    sleep $delay
 done
