a8ca98be45a8ad542cb891c7b00351f4ba92dcfe
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2022-04-03 13:26:44 +0200

    Updated code

--- oracle/setup.py
+++ oracle/setup.py
@@ -7,12 +7,29 @@ from Cython.Distutils import build_ext
 import numpy
 
 setup(
-cmdclass = {'build_ext': build_ext},
-ext_modules = [Extension("oracle",
-                         sources = ["oracle.pyx", "cpp_oracle.cpp"],
-                         language="c++",
-                         extra_compile_args = ["-Wno-cpp", "-Wno-unused-function","-Ofast", "-march=native", "-funroll-loops",
-                                               "-funsafe-loop-optimizations", "-falign-functions=16", "-falign-loops=16"],
-                         include_dirs = [numpy.get_include()])
-],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension(
+            "oracle",
+            sources=[
+                "oracle.pyx",
+                "cpp_oracle.cpp"
+            ],
+            language="c++",
+            extra_compile_args=[
+                "-Wno-cpp",
+                "-Wno-unused-function",
+                "-Wno-deprecated-declarations",
+                "-Wno-maybe-uninitialized",
+                "-Wno-stringop-overflow",
+                "-Ofast",
+                "-march=native",
+                "-funroll-loops",
+                "-funsafe-loop-optimizations",
+                "-falign-functions=16",
+                "-falign-loops=16",
+            ],
+            include_dirs=[numpy.get_include()]
+        )
+    ],
 )
